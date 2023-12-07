9882665d559012f7d8eff6552c5cca04d2e16162
juasruhiyat15 <juasruhiyat567@gmail.com>
2023-12-07 17:41:34 +0800

    Init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hellow ');
+});
+
+app.listen(3000, function(){
+    console.log('Server run on port 3000');
+});
