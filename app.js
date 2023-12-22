c4769cc37348f4d86e5abc41a2ecb3dbd89aa1ad
farhantaufiks <farhantaufiks@gmail.com>
2023-12-22 16:31:20 +0700

    added limit up to 10Mb

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./routes/index');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
+app.use(bodyParser.json({ extended: true, limit: "10mb" }));
 
-app.get('/', function (req, res) {
-    res.send('Hellow ');
+app.get("/", function (req, res) {
+  res.send("Hellow ");
 });
 
 app.use(cors());
 app.use(router);
 
-app.listen(4002, function () {
-    console.log('Server run on port 4002');
+app.listen(4003, function () {
+  console.log("Server run on port 4002");
 });
